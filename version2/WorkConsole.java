8e8e9e6441bcd4326c0167cd6ceafd5fe7b85c1b
EvgeniyChu <128834269+EvgeniyChu@users.noreply.github.com>
2023-05-09 18:42:11 +0400

    Create WorkConsole.java

---
+++ version2/WorkConsole.java
@@ -0,0 +1,18 @@
+public class WorkConsole {
+    public static void printGreetings() {
+        System.out.println("Давайте сыграем в игру 'Быки и коровы'");
+        System.out.println("Я загадал 4-х значное число без повторения цифр");
+        System.out.println("Вам необходимо ввести 4-х значное число");
+        System.out.println("Количество быков = полное совпадение Вашей цифры с цифрой компьютера");
+        System.out.println("Количество коров = Ваша цифра присутствует в загаданном числе");
+    }
+    public static void printRequest(String a, String b, String c, int x, int z) {
+        System.out.println("\tЗапрос: " + a + "\tОтвет: " + x + b + z + c);
+    }
+    public static void printGuessNumber(int x) {
+        System.out.print("Введите число " + x + ": ");
+    }
+    public static void printAmountTries (int x) {
+        System.out.println("\tСтрока была угадана за " + x + " попыток.");
+    }
+}
